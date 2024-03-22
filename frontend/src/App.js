abff303fe35b18e5febd898ca64bd81da69e86ef
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-03-22 23:00:44 +0800

    Set up React app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">All Bookstore</header>
+    </div>
+  );
+}
+
+export default App;
